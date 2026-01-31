ddcc05f410b4d8872925821893d1ca7b8a913137
Richard Amoako <andradeamoako612@gmail.com>
2026-01-31 10:14:38 +0000

    Add RIAMR class for Research Mode functionality

---
+++ src/modes/RSOF/src/modes/RIAM-R/index.js
@@ -0,0 +1,11 @@
+class RIAMR {
+    constructor() {
+        this.name = "RIAM-R (Research Mode)";
+    }
+
+    start() {
+        console.log(`[RIAM-R] ${this.name} is running.`);
+    }
+}
+
+module.exports = RIAMR;
