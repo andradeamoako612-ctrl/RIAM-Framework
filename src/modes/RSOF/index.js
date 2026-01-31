62942e587f8515603968cc81ff36097815fdd004
Richard Amoako <andradeamoako612@gmail.com>
2026-01-31 10:13:49 +0000

    Add RSOF class with start method

---
+++ src/modes/RSOF/index.js
@@ -0,0 +1,11 @@
+class RSOF {
+    constructor() {
+        this.name = "RSOF (General Mode)";
+    }
+
+    start() {
+        console.log(`[RSOF] ${this.name} is running.`);
+    }
+}
+
+module.exports = RSOF;
